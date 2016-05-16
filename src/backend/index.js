5e28868349fc9d80b802945ed156f7be74e9f8b1
Vyotiainen <ilpo.andstrom@gmail.com>
2016-05-16 11:46:57 +0300

    second milestone

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,14 +1,22 @@
 'use strict';
 
 var Percolator = require('percolator').Percolator;
+var dbSession = require('../../src/backend/dbSession.js');
 
 var port = 8080;
-var server = Percolator({'port':port});
+var server = Percolator({'port':port, 'autoLink': false});
 
 server.route('/api/keywords', 
     {
         GET: function(req, res) {
-            res.object({'foo':'bar'}).send();
+            dbSession.fetchAll('select id, value, categoryID from keyword Order by id', function(err, rows) {
+                if(err) {
+                    console.log(err);
+                    res.status.internalServerError(err);
+                } else {
+                    res.collection(rows).send();
+                }
+            });
         }
     }
 );
