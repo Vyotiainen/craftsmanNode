9f02594e96294ef83e3634b589529f3a8d319f6d
Vyotiainen <ilpo.andstrom@gmail.com>
2016-05-20 00:32:28 +0300

    milestone 5 reached

--- src/backend/server.js
+++ src/backend/server.js
@@ -5,12 +5,27 @@ var dbSession = require('../../src/backend/dbSession.js');
 
 var port = 8080;
 var Server = function(port) {
-    var server = Percolator({'port':port, 'autoLink': false});
+    var server = Percolator({'port':port, 'autoLink': false, "staticDir": __dirname+'/../frontend'});
 
     server.route('/api/keywords', 
         {
             GET: function(req, res) {
                 dbSession.fetchAll('select id, value, categoryID from keyword Order by id', function(err, rows) {
+                    if(err) {
+                        console.log(err);
+                        res.status.internalServerError(err);
+                    } else {
+                        res.collection(rows).send();
+                    }
+                });
+            }
+        }
+    );
+    
+    server.route('/api/keywords/categories',
+        {
+            GET: function(req,res) {
+                dbSession.fetchAll('select id, name from category order by id', function(err, rows) {
                     if(err) {
                         console.log(err);
                         res.status.internalServerError(err);
